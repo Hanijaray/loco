41a287159a3b695e59d63e569809dc5a8e800ab1
Hanijaray <131593552+Hanijaray@users.noreply.github.com>
2024-12-20 12:13:39 +0530

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const server = http.createServer(app);
 // Socket.io configuration with CORS to allow communication with the client
 const io = new Server(server, {
     cors: {
-        origin: ["http://localhost:5500", "http://127.0.0.1:5500"], // Allow both origins
+        origin: ["https://loco-zc5a.onrender.com"], // Allow both origins
         methods: ["GET", "POST"], // Allowed methods
     }
 });
